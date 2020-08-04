9fcdc5e4c924a897aa58af727411ba488d80765e
Essam <eemorsi@gmail.com>
2020-08-04 09:13:04 +0200

    init ray from the python and start simple actor process

--- getter.py
+++ getter.py
@@ -2,11 +2,17 @@ from sys import argv, exit
 import numpy as np
 import pdi
 import yaml
+import ray
 
 
 if __name__ == '__main__':
 
     config_path = "get.yml"
+    # precreated ray cluster configuration 
+    machine = "gros-16"
+    cluster = "nancy.grid5000.fr"
+    redis = ".".join([machine,cluster])
+    ray.init(address=":".join([redis, "16380"]))
 
     with open(config_path, 'r') as config_file:
         try:
@@ -19,6 +25,7 @@ if __name__ == '__main__':
     scalar = np.array(0)
     
     pdi.expose('wait', wait, pdi.IN)
+    # with each message passed through flowvr, create an actor job 
     while(wait != 0):
         pdi.expose('scalar', scalar, pdi.IN)
         print("PY scalar: {}".format(scalar))
