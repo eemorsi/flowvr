8873de46353b4079baf40f7d8ceafb4348da4b76
essam <essam.algizawy@brightskiesinc.com>
2020-09-04 19:10:10 +0200

    add flowvr deamon to run

--- couple/run.sh
+++ couple/run.sh
@@ -6,5 +6,6 @@ source /home/emorsi/pdi/build/bin/flowvr-suite-config.sh
 ray start --head --port=16380 --webui-host 0.0.0.0
 
 python3 tictac.py
+flowvrd &
 flowvr -p tictac &
 python3 getter.py $1 $2
