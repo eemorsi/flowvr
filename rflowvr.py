24617b0e1905600ab6c09907f116ed74778f93e4
essam <essam.algizawy@brightskiesinc.com>
2020-09-04 19:34:41 +0200

    clean files

--- rflowvr.py
+++
@@ -1,9 +0,0 @@
-from flowvrapp import *
-from pdi_flowvr import Module_PDI
-
-putmodule = Module_PDI("put", cmdline = "./cputter", pdi_conf = "put.yml")
-getmodule = Module_PDI("get", cmdline = "python3 getter.py grisou-33 nancy.grid5000.fr", pdi_conf = "get.yml")
-
-putmodule.getPort("text").link(getmodule.getPort("text"))
-        
-app.generate_xml("rflowvr")
